ea564c75a926b576a23e6b9d98f49f65dd31e4de
Matt Harnett <harnettmatt@gmail.com>
2017-10-02 21:48:26 -0700

    template list of bets

--- src/pages/bets/bets.ts
+++ src/pages/bets/bets.ts
@@ -6,9 +6,17 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'bets.html'
 })
 export class BetsPage {
+  bets: Array<{homeTeam: string, visitorTeam: string, time: string, date: string}>;
 
   constructor(public navCtrl: NavController) {
-
+    this.bets =[];
+    for(let i = 1; i<9; i++) {
+      this.bets.push({
+        homeTeam: 'Home Team ' + i,
+        visitorTeam: 'Visitor Team ' + i,
+        time: i + ':00',
+        date: i + '/' + i + '/200' + i
+      })
+    }
   }
-
 }
