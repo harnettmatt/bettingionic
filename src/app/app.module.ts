6a625ffd8fc4ae801cfd1ada7bcdbfc5087cc9e8
Matt Harnett <harnettmatt@gmail.com>
2017-10-09 20:00:57 -0700

    Bets API call and stub for Fixtures
    also added the layout for bets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
@@ -10,6 +11,8 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { FixtureServiceProvider } from '../providers/fixture-service/fixture-service';
+import { BetServiceProvider } from '../providers/bet-service/bet-service';
 
 @NgModule({
   declarations: [
@@ -21,6 +24,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
@@ -34,7 +38,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    FixtureServiceProvider,
+    BetServiceProvider
   ]
 })
 export class AppModule {}
--- src/pages/bets/bets.ts
+++ src/pages/bets/bets.ts
@@ -1,22 +1,35 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {BetServiceProvider} from '../../providers/bet-service/bet-service';
 
 @Component({
   selector: 'page-bets',
   templateUrl: 'bets.html'
 })
 export class BetsPage {
-  bets: Array<{homeTeam: string, visitorTeam: string, time: string, date: string}>;
+  bets: any;
+  fixtures: any;
 
-  constructor(public navCtrl: NavController) {
-    this.bets =[];
+  constructor(public betService: BetServiceProvider) {
+    this.fixtures = []
+    this.fetchFixtures();
+    this.fetchBets();
+  }
+
+  fetchFixtures(){
     for(let i = 1; i<9; i++) {
-      this.bets.push({
+      this.fixtures.push({
         homeTeam: 'Home Team ' + i,
         visitorTeam: 'Visitor Team ' + i,
         time: i + ':00',
         date: i + '/' + i + '/200' + i
       })
     }
+  }
+
+  fetchBets(){
+    this.betService.load().then(data => {
+      this.bets = data;
+    });
   }
 }
