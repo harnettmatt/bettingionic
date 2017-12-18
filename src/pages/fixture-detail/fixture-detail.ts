3448823ee479fe7ecf5d502b9009a5906e04c3fe
Matt Harnett <harnettmatt@gmail.com>
2017-12-17 18:54:20 -0800

    images and date/time with the ability to select an image

--- src/pages/fixture-detail/fixture-detail.ts
+++ src/pages/fixture-detail/fixture-detail.ts
@@ -1,12 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the FixtureDetailPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+
 
 @IonicPage()
 @Component({
@@ -15,8 +10,25 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class FixtureDetailPage {
   fixture: any;
+  betable1Photo: any;
+  betable2Photo: any;
+  selectedBetable: any;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.fixture = navParams.get('fixture');
+    this.betable1Photo = this.fixture.betable1.grayscalePhoto;
+    this.betable2Photo = this.fixture.betable2.grayscalePhoto;
+  }
+
+  selectBetable1() {
+    this.betable1Photo = this.fixture.betable1.colorPhoto;
+    this.betable2Photo = this.fixture.betable2.grayscalePhoto;
+    this.selectedBetable = this.fixture.betable1;
+  }
+
+  selectBetable2() {
+    this.betable2Photo = this.fixture.betable2.colorPhoto;
+    this.betable1Photo = this.fixture.betable1.grayscalePhoto;
+    this.selectedBetable = this.fixture.betable2;
   }
 }
