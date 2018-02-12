f8f32269034de16f80bf1ae6f02acde7b98eb41a
Matt Harnett <harnettmatt@gmail.com>
2018-02-11 23:15:48 -0800

    layout is complete and data is available to the view

--- src/pages/bet-detail/bet-detail.ts
+++ src/pages/bet-detail/bet-detail.ts
@@ -15,8 +15,41 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class BetDetailPage {
   bet: any;
+  user: any;
+  betable1Photo: any;
+  betable2Photo: any;
+  userRole: any;
+  friend: any;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.bet = navParams.get('bet');
+    console.log(this.bet);
+    this.user = JSON.parse(window.localStorage.getItem('user'));
+    this.determineSides();
+  }
+
+  determineSides() {
+    if (this.user.id == this.bet.proposer.id) {
+      this.friend = this.bet.acceptor
+      if (this.bet.proposerSide.id == this.bet.fixture.betable1.id) {
+        this.betable1Photo = this.bet.proposerSide.colorPhoto;
+        this.betable2Photo = this.bet.acceptorSide.grayscalePhoto;
+      }
+      else {
+        this.betable1Photo = this.bet.acceptorSide.grayscalePhoto;
+        this.betable2Photo = this.bet.proposerSide.colorPhoto;
+      }
+    }
+    else {
+      this.friend = this.bet.acceptor
+      if (this.bet.acceptorSide.id == this.bet.fixture.betable1.id) {
+        this.betable1Photo = this.bet.acceptorSide.colorPhoto;
+        this.betable2Photo = this.bet.proposerSide.grayscalePhoto;
+      }
+      else {
+        this.betable1Photo = this.bet.proposerSide.grayscalePhoto;
+        this.betable2Photo = this.bet.acceptorSide.colorPhoto;
+      }
+    }
   }
 }
